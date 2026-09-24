91b1d5fe6a2203ee07e6a46871694b38548b36c6
AzulinhoZz <alexisremo24@gmail.com>
2026-09-24 13:18:02 -0600

    fix(core-2.0): derive tenant context in scoped controllers

--- server/src/modules/core/branches/controller.js
+++ server/src/modules/core/branches/controller.js
@@ -1,10 +1,12 @@
 'use strict';
 
+const { requireTenant } = require('../../../middlewares/tenantScope');
+
 const service = require('./service');
 
 async function list(req, res, next) {
   try {
-    const companyId = req.query.companyId || req.user.companyId;
+    const companyId = requireTenant(req);
     res.json(await service.list({ companyId }));
   } catch (err) {
     next(err);
@@ -13,7 +15,7 @@ async function list(req, res, next) {
 
 async function getById(req, res, next) {
   try {
-    res.json(await service.getById(req.params.id));
+    res.json(await service.getById(req.params.id, requireTenant(req)));
   } catch (err) {
     next(err);
   }
@@ -21,8 +23,8 @@ async function getById(req, res, next) {
 
 async function create(req, res, next) {
   try {
-    const body = { ...req.body };
-    if (!body.companyId) body.companyId = req.user.companyId;
+    const body = { ...req.body, companyId: requireTenant(req) };
+    body.companyId = requireTenant(req);
     res.status(201).json(await service.create(body));
   } catch (err) {
     next(err);
@@ -31,7 +33,7 @@ async function create(req, res, next) {
 
 async function update(req, res, next) {
   try {
-    res.json(await service.update(req.params.id, req.body));
+    res.json(await service.update(req.params.id, { ...req.body, companyId: requireTenant(req) }, requireTenant(req)));
   } catch (err) {
     next(err);
   }
